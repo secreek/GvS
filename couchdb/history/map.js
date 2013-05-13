f17657ecf2f8e217bc7e736d15d5503def084d27
Void Main <voidmain1313113@gmail.com>
2013-05-13 12:18:25 +0800

    When calculating hotness, only take PushEvent into consideration

--- couchdb/history/map.js
+++ couchdb/history/map.js
@@ -3,11 +3,13 @@ function(doc){
     for (var i = 0; i < doc["data"].length; i++) {
       var evt = doc["data"][i];
       var type = evt["type"];
-      var actor = evt["actor"]["login"];
-      var avatar = evt["actor"]["avatar_url"];
-      var date = evt["created_at"];
-      var result = { "date" : date, "type" : type };
-      emit(actor + "/" + avatar, result);
+      if(type === "PushEvent") {
+        var actor = evt["actor"]["login"];
+        var avatar = evt["actor"]["avatar_url"];
+        var date = evt["created_at"];
+        var result = { "date" : date, "count" : evt["payload"]["commits"].length };
+        emit(actor + "/" + avatar, result);
+      }
     }
   }
 }
